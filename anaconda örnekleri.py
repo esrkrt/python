e56021a7160fdca6c2b17a208c71c2eb872690ae
esrkrt <47241960+esrkrt@users.noreply.github.com>
2019-11-05 21:37:25 +0200

    Add files via upload

---
+++ anaconda örnekleri.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Kullanıcıdan bir dik üçgenin dik olan iki kenarı (a,b)alıp  
+hipotenüs uzunluğu bulmaya çalışılıyor
+
+"""
+a=int(input("a;"))
+b=int(input("b;"))
+c=(a**2+b**2)**0.5
+print("hipotenüs;",c)
